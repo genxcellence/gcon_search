1f89f10cb563006677c7bf6288bd8f04efef38c6
Kunal Malviya <kunal.malviya@msquare.media>
2023-09-14 14:22:09 +0530

    added security configuration

--- src/main/java/com/gcon/search/configuration/ElasticSearchConfig.java
+++ src/main/java/com/gcon/search/configuration/ElasticSearchConfig.java
@@ -1,26 +1,34 @@
 package com.gcon.search.configuration;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
 @ComponentScan
-
+@EnableElasticsearchRepositories
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+
+    @Value("${elasticsearch.host}")
+    private String host;
+    @Value("${elasticsearch.username}")
+    private String username;
+    @Value("${elasticsearch.password}")
+    private String password;
 
     @Bean
     @Override
     public RestHighLevelClient elasticsearchClient()  {
-           ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
-                .build();
+        final ClientConfiguration clientConfiguration =
+                ClientConfiguration.builder().connectedTo(host)
+                        .withBasicAuth(username, password).build();
 
-        return RestClients.create(clientConfiguration)
-                .rest();
+        return RestClients.create(clientConfiguration).rest();
 
     }
 
