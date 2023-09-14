1467656567164384eb4328f5e39006da431077af
Kunal Malviya <kunal.malviya@msquare.media>
2023-09-14 23:05:11 +0530

    added swagger configuration

--- src/main/java/com/gcon/ElasticSearchApplication.java
+++ src/main/java/com/gcon/ElasticSearchApplication.java
@@ -2,15 +2,37 @@ package com.gcon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @EnableElasticsearchRepositories
+@EnableSwagger2
 public class ElasticSearchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElasticSearchApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(getInfo())
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
+
+	public ApiInfo getInfo() {
+		return new ApiInfo("GCON_ELASTIC_SEARCH", "GCON_ELASTIC_SEARCH", "1.0", "", "GCON_ELASTIC_SEARCH", "", "");
 	}
 
 }
